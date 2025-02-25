c90431e138ffa32f547f89dcb009d08f893a1e09
oumaima <omaimaelbdraouy@gmail.com>
2025-02-25 15:12:17 +0000

    nice

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Permet d'exposer l'application sur le réseau
+    port: 5173, // Utilise le même port que celui de Vite
+  },
+  preview: {
+    host: true,
+    port: 4173, // Port par défaut pour `vite preview`
+    allowedHosts: ["quiz-with-react-wegb.onrender.com"], // Autorise l'hôte de Render
+  }
 })
